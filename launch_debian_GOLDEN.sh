88ffeee5ab7b30c5ec8f75e6c1384fe002654cf6
Picta Ricta <picta.ricta@gmail.com>
2026-02-12 18:16:40 -0600

    golden script updated

--- launch_debian_GOLDEN.sh
+++ launch_debian_GOLDEN.sh
@@ -14,21 +14,21 @@ if [ -n "$PENDING" ]; then
     echo "Pending Pods detected: $PENDING"
     echo "Launching new VM..."
     RANDOM_SUFFIX=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
-    sudo cp /var/lib/libvirt/images/debian-12-generic-arm64.qcow2_bkup_resized /var/lib/libvirt/images/vm-$RANDOM_SUFFIX.qcow2
     VM_NAME="k8s-node-$RANDOM_SUFFIX"
     FQDN="$VM_NAME.example.com"
+    sudo cp /var/lib/libvirt/images/debian-12-generic-arm64.qcow2_bkup_resized /var/lib/libvirt/images/vm-$VM_NAME.qcow2
     export VM_NAME FQDN
-    envsubst '$VM_NAME $FQDN' < /var/lib/libvirt/images/data.yaml | sudo tee /var/lib/libvirt/images/data-$RANDOM_SUFFIX.yaml > /dev/null
+    envsubst '$VM_NAME $FQDN' < /var/lib/libvirt/images/data.yaml | sudo tee /var/lib/libvirt/images/data-$VM_NAME.yaml > /dev/null
 
     sudo virt-install \
       --name "$VM_NAME" \
       --memory 512 \
       --vcpus 1 \
-      --disk path=/var/lib/libvirt/images/vm-$RANDOM_SUFFIX.qcow2,size=20 \
+      --disk path=/var/lib/libvirt/images/vm-$VM_NAME.qcow2,size=20 \
       --import \
       --osinfo detect=on,require=off \
       --network network=br0 \
-      --cloud-init root-password-file=/var/lib/libvirt/images/pass.txt,user-data=/var/lib/libvirt/images/data-$RANDOM_SUFFIX.yaml,disable=on \
+      --cloud-init root-password-file=/var/lib/libvirt/images/pass.txt,user-data=/var/lib/libvirt/images/data-$VM_NAME.yaml,disable=on \
       --graphics none
 
 else
